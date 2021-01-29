ffa4b00a33fdaddca1eeef31140ec148b272fe14
Melanie Hall <hallmt313@gmail.com>
2021-01-29 11:28:49 -0500

    my business card

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# npx-mhall313
+Hi, I'm Melanie!
+
+# Usage
+
+```bash
+npx mhall313
+```
+
+# Credit
+
+Forked from [@bitandbang](https://twitter.com/bitandbang/status/1075473070368919552)
+([repo](https://github.com/bnb/bitandbang)) via [@mxstbr](https://github.com/mxstbr/)
---
+++ bin/card.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Melanie Hall /'),
+  handle: chalk.cyan('mhall313'),
+  work: chalk.white('Financial Analyst at Capital One'),
+  github: chalk.cyan('github.com/mhall313'),
+  linkedin: chalk.cyan('linkedin.com/in/hallmelanie/'),
+  web: chalk.cyan('https://melaniehall-portfolio.netlify.app/'),
+  npx: chalk.white('npx melaniehall'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+  labelCard: chalk.white.bold('      Card:'),
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding
+
+console.log(chalk.green(boxen(output, options)))
